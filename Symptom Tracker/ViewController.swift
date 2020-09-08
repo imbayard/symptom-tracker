36bb7f9afc6f578756cbd1089cf4f2bd8aa86795
Bayard Eton <beton@bu.edu>
2020-09-08 19:09:12 -0400

    Initial Commit

---
+++ Symptom Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Symptom Tracker
+//
+//  Created by Bayard Eton on 9/8/20.
+//  Copyright © 2020 Bayard Eton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
