a9d67a5a44587bd327369ace675e874e9d185d73
Bayard Eton <beton@bu.edu>
2020-09-10 16:23:22 -0400

    Added communication with database and notes on where to proceed

--- Symptom Tracker/ViewController.swift
+++ Symptom Tracker/ViewController.swift
@@ -11,6 +11,16 @@ import GoogleSignIn
 import Firebase
 import FirebaseDatabase
 
+// Typical user flow:
+//         Login --> Dashboard --> Symptom Tracker --> Dashboard --> Data page (with API)
+//         Login --> Dashboard --> Data page (with API)
+// Segues:
+//         Login --> Dashboard
+//         Data --> Dashboard
+//         Symptom Tracker --> Dashboard
+//         Dashboard --> Symptom Tracker
+//         Dashboard --> Data page
+
 class ViewController: UIViewController, GIDSignInDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,36 +35,55 @@ class ViewController: UIViewController, GIDSignInDelegate {
             print(error)
             return
         }
-        print(user.profile.email)
-        performSegue(withIdentifier: "Homepage", sender: ViewController())
+        print(user.profile.email) // Instead of printing we want to send this to database
+        performSegue(withIdentifier: "Login_To_Dash", sender: ViewController())
     }
 }
 
-class HomePage_ViewController: UIViewController {
+class SymptomTracker_ViewController: UIViewController {
+    
+    //Could pull from database and check if user has logged symptoms for the day already
+    
     // Create database reference
     var refSymptomDb: DatabaseReference!
     // References to the symptom segment sliders in HomePage_ViewController
-    @IBOutlet weak var CoughSymptom: UISegmentedControl!
+    @IBOutlet weak var cough_switch: UISwitch!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        refSymptomDb = Database.database().reference().child("symptoms")
     }
     
     // The 'on click' function triggered after user presses submit
-    @IBAction func SubmitSymptoms(_ sender: UIButton) {
+
+    @IBAction func submitSymptoms(_ sender: UIButton) {
         addSymptoms()
     }
     
+    
     // Record the symptoms in the database
     func addSymptoms() {
-        //let key = refSymptomDb.childByAutoId().key
+        // Need to find some way of ensuring an account can only record symptoms once per day
+        // Research user privileges, account privileges, etc with firebase
+        let key = refSymptomDb.childByAutoId().key
         let symptoms = [
-            //"Cough": CoughSymptom.titleForSegment(at: CoughSymptom.selectedSegmentIndex),
-            "Cough": CoughSymptom.selectedSegmentIndex
-        ]
+            "key": key,
+            "cough": cough_switch.isOn,
+            ] as [String : Any]
         print(symptoms)
+        refSymptomDb.child(key ?? "abc").setValue(symptoms)
+        
+        // Symptoms:
+        //     Fever (100 deg)
+        //     Cough
+        //     Shortness of breath / difficulty breathing
+        //     Sore throat
+        //     New loss of taste or smell
+        //     Vomiting
+        //     Severe fatigue
+        //     Severe muscle aches
         
         // We want to
         //            1) Pull from database and get current number of "Cough", "Fever", etc
@@ -62,6 +91,15 @@ class HomePage_ViewController: UIViewController {
         //               i.e. if User enters yes for cough, the cough value gets +1
         //            3) Push the new value back to the database
         //            4) (Maybe?) Error check to see that the new value is correct
+        
     }
+    
+}
+
+class HomePage_ViewController: UIViewController {
+    
+    //Need to move code from here --> Another View Controller called "Symptom Tracker"
+    
+
 }
 
